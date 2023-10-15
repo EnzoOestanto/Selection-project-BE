94360d3b595a53eaf7ff3a2a08eb0afbd69049f7
Putra Utama Jaya <putrautamajaya@gmail.com>
2023-10-15 13:27:58 +0700

    Final

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -3,84 +3,84 @@ const likeDB = db.like
 
 
 module.exports = {
-    addLike: async (req, res) => {
-        try {
-            const { postId, userId } = req.body
-            const response = await likeDB.create({ post_id: postId, user_id: userId })
+  addLike: async (req, res) => {
+    try {
+      const { postId, userId } = req.body
+      const response = await likeDB.create({ post_id: postId, user_id: userId })
 
-            return res.status(201).send({
-                status: 201,
-                success: true,
-                message: 'like success',
-                data: response,
-            })
+      return res.status(201).send({
+        status: 201,
+        success: true,
+        message: 'like success',
+        data: response,
+      })
 
-        } catch (error) {
-            res.send({
-                status: error.status,
-                success: error.success,
-                message: error.message
-            })
-        }
-    },
-    getLikes: async (req, res) => {
-        try {
-            console.log('masuk get likes')
-            const { postId, userId } = req.query
-            const totalLikes = await likeDB.count({
-                where: {
-                    post_id: postId,
-                }
-            })
-            const response = await likeDB.count({
-                where: {
-                    post_id: postId,
-                    user_id: userId
-                }
-            })
-
-            return res.status(200).send({
-                status: 200,
-                success: true,
-                message: 'get likes sucess',
-                totalLikes: totalLikes,
-                data: response
-
-            })
-
-        } catch (error) {
-
-        }
-    },
-    deleteLike: async (req, res) => {
-        try {
-
-            const { postId, userId } = req.query
-            const response = await likeDB.destroy({
-                where: {
-                    post_id: postId,
-                    user_id: userId
-                }
-            })
-            console.log('delete',response)
-
-            if (response > 0) {
-                return res.status(200).send({
-                    status: 200,
-                    success: true,
-                    message: 'unlike success',
-                    data: response
-
-                })
-            }
-        } catch (error) {
-            res.send({
-                status: error.status,
-                success: error.success,
-                message: error.message,
-                data: []
-            })
-
-        }
+    } catch (error) {
+      res.send({
+        status: error.status,
+        success: error.success,
+        message: error.message
+      })
     }
+  },
+  getLikes: async (req, res) => {
+    try {
+      console.log('masuk get likes')
+      const { postId, userId } = req.query
+      const totalLikes = await likeDB.count({
+        where: {
+          post_id: postId,
+        }
+      })
+      const response = await likeDB.count({
+        where: {
+          post_id: postId,
+          user_id: userId
+        }
+      })
+
+      return res.status(200).send({
+        status: 200,
+        success: true,
+        message: 'get likes sucess',
+        totalLikes: totalLikes,
+        data: response
+
+      })
+
+    } catch (error) {
+
+    }
+  },
+  deleteLike: async (req, res) => {
+    try {
+
+      const { postId, userId } = req.query
+      const response = await likeDB.destroy({
+        where: {
+          post_id: postId,
+          user_id: userId
+        }
+      })
+      console.log('delete', response)
+
+      if (response > 0) {
+        return res.status(200).send({
+          status: 200,
+          success: true,
+          message: 'unlike success',
+          data: response
+
+        })
+      }
+    } catch (error) {
+      res.send({
+        status: error.status,
+        success: error.success,
+        message: error.message,
+        data: []
+      })
+
+    }
+  }
 }
